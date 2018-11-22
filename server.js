89fdce85423daeaf4e3c49b8d3f22d243f37b752
Max GERBER <mgerber@c2r5s6.wethinkcode.co.za>
2018-11-22 11:36:01 +0200

    Adding to database, need to add hashed passwords

--- server.js
+++ server.js
@@ -6,66 +6,79 @@
     npm install mysql               --save
     npm install express-validator   --save
     npm install bcrypt              --save
-
-    ./mongod --dbpath ~/mongo-data
+    npm install mongodb@2.2.5          --save
 */
 
-const express =     require('express');                             //Include Express
-const ejs =         require('ejs');                                 //Include EJS
-const bodyParser =  require('body-parser');                         //body-parser
-const mysql =       require('mysql');                               //mySQL
+const express =     require('express');                                 //Include Express
+const ejs =         require('ejs');                                     //Include EJS
+const bodyParser =  require('body-parser');                             //body-parser
+const mysql =       require('mysql');                                   //mySQL
 const bcrypt =      require('bcrypt');
+const {MongoClient, ObjectID} = require('mongodb');                     //MongoDB (deconstructed)
 
 const app = express();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false});   //Body-Parser
-
-app.use(express.static(__dirname + '/public'));                     //Add Public Folder (CSS)
-app.set('view engine', 'ejs');                                      //Set View Engine to EJS
-
-//================================================================  DATABASE
-
-//================================================================  DATABASE
-                                                                 
-var form = "";                                                      //Declare some Variables
-const saltRounds = 1;
-
-app.get("/", function(req, res) {                                   //Get Root
-    const name = "TREDX";
-    form = "1";
-    res.render("index", {name, form});
-})
-
-app.post("/1", urlencodedParser, function(req, res) {               //GET POST REQUEST
-    form = "1";
-    console.log(req.body);
-
-    const errors = validate(req);
-
-    if (errors.length == 0) {
-
-        bcrypt.hash(req.body.upsw, saltRounds, function(err, hash){ //Encrypt The Password with bcrypt
-            req.body.upsw = hash;
-            res.render("index-logged", {data: req.body});
-          });
-        
-    } else {
-        console.log(errors);
+MongoClient.connect('mongodb://localhost:27017', (err, db) => {         //Connect to database
+    if(err){
+        return console.log('Unable to connect beacuse: '+err);          //Connection error
     }
-})
+    console.log('Connected');
 
-function validate(req) {                                            //Validate Email And Password - Only checking if entered
-    const errors = [];
+    var urlencodedParser = bodyParser.urlencoded({ extended: false});   //Body-Parser
 
-    if (!req.body.uname) {                                          //uname
-        errors.push("Email Has Not Been Entered!");
+    app.use(express.static(__dirname + '/public'));                     //Add Public Folder (CSS)
+    app.set('view engine', 'ejs');                                      //Set View Engine to EJS
+
+    var access = "";                                                    //Declare some Variables
+    const saltRounds = 1;
+
+    app.get("/", function(req, res) {                                   //Get Root
+        const name = "TREDX";
+        access = "1";
+        res.render("index", {name, access});
+    })
+
+    app.post("/1", urlencodedParser, function(req, res) {               //GET POST REQUEST
+        access = "1";
+        console.log(req.body);
+
+        const errors = validate(req);
+
+        if (errors.length == 0) {
+            bcrypt.hash(req.body.upsw, saltRounds, function(err, hash){ //Encrypt The Password with bcrypt
+                if (err){
+                    return console.log('Unable to hash', err);
+                }
+                req.body.upsw = hash;
+                res.render("index-logged", {data: req.body});
+            });
+            db.collection('Users').insertOne({                          //Insert user into database
+                username: req.body.uname,
+                password: req.body.upsw                                 //Must figure out how to add HASHED password
+            }, (err, result) => {
+                if (err){
+                    return console.log('Unable to insert', err);
+                }
+                console.log(JSON.stringify(result.ops, undefined, 2));  //Inserted documents
+            });
+        } else {
+            console.log(errors);
+        }
+    })
+    
+    function validate(req) {                                            //Validate Email And Password - Only checking if entered
+        const errors = [];
+
+        if (!req.body.uname) {                                          //uname
+            errors.push("Email Has Not Been Entered!");
+        }
+        if (!req.body.upsw) {                                           //upsw
+            errors.push("Password Has Not Been Entered!");
+        }
+        return errors;
     }
-    if (!req.body.upsw) {                                           //upsw
-        errors.push("Password Has Not Been Entered!");
-    }
-    return errors;
-}
 
-app.listen(8080, function() {                                       //Shh and listen!
-    console.log("Listen on 8080");
-})
+    app.listen(8080, function() {                                       //Shh and listen!
+        console.log("Listen on 8080");
+    })
+});
