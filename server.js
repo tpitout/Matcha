4d37978a74853d363c557cd74d9f75743b712bb8
Tredeaux PITOUT <tpitout@c2r3s5.wethinkcode.co.za>
2018-11-30 11:04:29 +0200

    Fresh_Clean

--- server.js
+++ server.js
@@ -12,6 +12,8 @@
     npm install sweetalert          --save
 
     Run XAMPP or MAMP APACHE/MySQL
+
+    Modal Errorirect
 */
 
 const express =                 require('express');                     //Include Express
@@ -28,21 +30,8 @@ const swal =                    require('sweetalert');
 const app = express();
 const urlencodedParser = bodyParser.urlencoded({ extended: false});
 
-const mailOptions = {
-    from: 'official.matcha@gmail.com',
-    to: req.body.uemail,
-    subject: 'WELCOME TO MATCHA ❤️',
-    html: '<div style="border: 5px SOLID #FF5864"><h1 style="color:#FF5864;text-align:center;">WELCOME TO MATCHA</h1> <h2 style="font-size:30px;color:#FF5864;text-align:center;">'+req.body.ufname+" "+req.body.ulname+
-    "<br><a style='font-size:20px;text-align:center;color:white;text-decoration:none;background-color:#FF5864;padding: 5px 5px;' href='http://localhost:8080/profile_setup.mp3?token="+token+"'>LOGIN</a>"+"</div>"
-};
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'official.matcha@gmail.com',
-      pass: 'MatchaMatcha'
-    }
-});
+var red = "❌ \x1b[1m \x1b[31m";
+var green = "✅ \x1b[1m \x1b[32m";
 
 app.use(express.static(__dirname + '/public'));                        
 app.set('view engine', 'ejs');   
@@ -57,13 +46,13 @@ con.connect(function(err) {
     if (err){
         console.log(err);
     }
-    console.log("✅ \x1b[1m \x1b[32m CONNECTED TO MySQL \x1b[33m PORT: 3030 \x1b[0m");
+    console.log(green+ " CONNECTED TO MySQL \x1b[33m PORT: 3030 \x1b[0m");
     con.query("CREATE DATABASE maindata", function (err, result) {
         if (err){
-            console.log("❌ \x1b[1m \x1b[31m "+err+" \x1b[0m");
+            console.log(red+err+" \x1b[0m");
             return;
         }
-        console.log("✅ \x1b[1m \x1b[32m DATABASE CREATED : \x1b[33m MAINDATA \x1b[0m");
+        console.log(green + "DATABASE CREATED : \x1b[33m MAINDATA \x1b[0m");
         var con = mysql.createConnection({
             host: "localhost",
             user: "root",
@@ -73,39 +62,48 @@ con.connect(function(err) {
 
         con.connect(function(err) {
             if (err){
-                console.log("❌ \x1b[1m \x1b[31m DATABASE ALREADY EXIST'S \x1b[0m");
+                console.log(red+ " DATABASE ALREADY EXIST'S \x1b[0m");
                 return;
             }
-            console.log("✅ \x1b[1m \x1b[32m CONNECTED TO MySQL \x1b[33m PORT: 3030 \x1b[0m");
+            console.log(green +" CONNECTED TO MySQL \x1b[33m PORT: 3030 \x1b[0m");
             var sql = "CREATE TABLE userdata (`id` INT AUTO_INCREMENT PRIMARY KEY, `username` VARCHAR(255), `name` VARCHAR(255), `surname` VARCHAR(255), `email` VARCHAR(255), `password` VARCHAR(255), `code` VARCHAR(4), `token` VARCHAR(255), `verified` TINYINT(1) DEFAULT '0', `reports` INT(5), `bio` TEXT, `tags` TEXT)";
             con.query(sql, function (err, result) {
                 if (err){
                     console.log(err);
                 }
-                console.log("✅ \x1b[1m \x1b[32m TABLE CREATED  :   \x1b[33m USERDATA \x1b[0m");
+                console.log(green +"TABLE CREATED  :   \x1b[33m USERDATA \x1b[0m");
             });
         });
     });
+});
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'official.matcha@gmail.com',
+      pass: 'MatchaMatcha'
+    }
 });
 
 app.get("/", function(req, res) {                                     
     res.render("index");
 });
 
-app.post("/", urlencodedParser, function(req, res) {                  
+app.post("/", urlencodedParser, function(req, res) {   
+    var success = "/";               
     console.log(req.body);
     con.query('SELECT `password` from `maindata`.`userdata` WHERE `username`= ?', [req.body.uname], function(err, result, fields) {
     if (err) {
-        console.log('Error while performing Query1.')
+        console.log(red + 'Error while performing Query1.')
     }
     else {
-        bcrypt.compare(req.body.upsw, result[0].password, (err, res) => {
-            if (res == true){
-                console.log('✅ \x1b[1m \x1b[32m Successfully Logged in! \x1b[0m ');
-
+        bcrypt.compare(req.body.upsw, result[0].password, (err, response) => {
+            if (response == true){
+                console.log(green+" Successfully Logged in! \x1b[0m ");
+                res.redirect("/main.txt");
             }
             else {
-                console.log('❌ \x1b[1m \x1b[31m Unuccessfully Logged in! \x1b[0m ');
+                console.log(red+"Unuccessfully Logged in! \x1b[0m ");
             }
         });
     }
@@ -117,49 +115,48 @@ app.get("/register.jpeg", function(req, res) {
 });
 
 app.post("/register", urlencodedParser, function(req, res) {
-    swal("hello");
     console.log(req.body);
     const errors = validateReg(req);
     if (errors.length == 0) {
         bcrypt.hash(req.body.upsw, 8, (err, hash) => {
             if (err){
-                return console.log("❌ \x1b[1m \x1b[31m UNABLE TO HASH \x1b[0m", err);
+                return console.log(red + " UNABLE TO HASH \x1b[0m", err);
             }
             token = crypto.randomBytes(16).toString(`hex`);
             con.query('SELECT * FROM `maindata`.`userdata` WHERE `username` = ?', [req.body.uname], (err, results, fields) => {
                 if (err) {
-                    console.log('❌ \x1b[1m \x1b[31m ERROR ON QUERY #2 \x1b[0m');
+                    console.log(red + 'ERROR ON QUERY #2 \x1b[0m');
                 }
                 if (results.length == 0){
                     console.log(req.body.email);
                     con.query('SELECT * FROM `maindata`.`userdata` WHERE `email` = ?', [req.body.uemail], (err, results, fields) => {
                         if (err) {
-                            console.log('❌ \x1b[1m \x1b[31m ERROR ON QUERY #3 \x1b[0m');
+                            console.log(red + 'ERROR ON QUERY #3 \x1b[0m');
                         }
                         if (results.length == 0){
                             con.query('INSERT INTO `maindata`.`userdata` (`name`, `surname`, `email`, `username`, `password`, `token`) VALUES (?,?,?,?,?,?)', [req.body.ufname, req.body.ulname, req.body.uemail, req.body.uname, hash, token], function(err, result, fields){
                                 if (err) {
-                                    console.log('❌ \x1b[1m \x1b[31m ERROR ON QUERY #4 \x1b[0m');
+                                    console.log(red + 'ERROR ON QUERY #4 \x1b[0m');
                                 }
                                 else {
-                                    console.log('✅ \x1b[1m \x1b[32m SUCCESFULLY ADDED NEW USER! \x1b[0m');
+                                    console.log(green + ' SUCCESFULLY ADDED NEW USER! \x1b[0m');
                                     transporter.sendMail(mailOptions, function(error, info){
                                         if (error) {
                                             console.log(error);
                                         } else {
-                                            console.log('✅ \x1b[1m \x1b[32m EMAIL SENT: \x1b[0m' + info.response);
+                                            console.log(green + ' EMAIL SENT: \x1b[0m' + info.response);
                                         }
                                     });
                                 }
                             });
                         }
                         else {
-                            console.log('❌ \x1b[1m \x1b[31m EMAIL EXISTS IN DATABASE \x1b[0m');
+                            console.log(red + 'EMAIL EXISTS IN DATABASE \x1b[0m');
                         }
                     })
                 }
                 else {
-                    console.log('❌ \x1b[1m \x1b[31m USERNAME EXISTS IN DATABASE \x1b[0m');
+                    console.log(red + 'USERNAME EXISTS IN DATABASE \x1b[0m');
                 }
             })
         })
@@ -170,17 +167,10 @@ app.post("/register", urlencodedParser, function(req, res) {
 
 app.post("/profile_setup", urlencodedParser, function(req, res) {
     console.log(req.body);
-    var code = evaluateCode(req.body.gender, req.body.pref);
-    var token = req.query.token;
-    con.query('INSERT INTO `maindata`.`userdata` (`code`, `bio`, `tags`) VALUES (?,?,?) WHERE `token` = ?', [code, req.body.bio, req.body.tags, token], (err, result, fields) => {
-        if (err) {
-            console.log('❌ \x1b[1m \x1b[31m ERROR ON QUERY #5 \x1b[0m', err);
-        }
-        else {
-            console.log('✅ \x1b[1m \x1b[32m SUCCESFULLY ADDED USER PREFERENCES! \x1b[0m');
-        }
-        });
-    console.log(token);
+    if (req.body)
+    {
+        //SQL INSERT
+    }
 });
 
 app.get("/profile_setup.mp3", function(req, res) {
@@ -192,59 +182,13 @@ app.get("/main.txt", function(req, res) {
 });
 
 app.listen(8080, function() {                                         
-    console.log("✅ \x1b[1m \x1b[32m SERVER LAUNCHED :: \x1b[33m PORT: 8080 \x1b[0m");
+    console.log(green + "SERVER LAUNCHED :: \x1b[33m PORT: 8080 \x1b[0m");
 });
 
 function validateReg(req) {
     const errors = [];
-
     if (req.body.upsw != req.body.upsw2) {
         errors.push("Passwords Do not match!");
     }
     return errors;
 };
-
-function evaluateCode(gender, preference) {
-    var code;
-
-    if (gender == "male"){
-        code = "01";
-    }
-    else {
-        code = "10";
-    }
-    if (preference == "none"){
-        code = code+"00";
-    }
-    else if (preference == "male"){
-        code = code+"01";
-    }
-    else if (preference == "female"){
-        code = code+"10";
-    }
-    else if (preference == "both"){
-        code = code+"11";
-    }
-    return code;
-}
-
-/*//Matcha matching
-
-u1 = parseInt("0101", 2);
-u2 = parseInt("1001", 2);
-
-// u1Pref = u1 & 3
-// u1Gend = u1 >> 2;
-
-// u2Pref = u2 & 3;
-// u2Gend = u2 >> 2;
-
-// preMatch1 = u1Gend & u2Pref;
-// finMatch1 = prefMatch1 >> 1 | prefMatch1 & 1;
-
-// preMatch2 = u2Gend & u1Pref;
-// finMatch2 = preMatch2 >> 1 | preMatch2 & 1;
-
-// result = finMatch1 & finMatch2;
-
-console.log(((((u1 >> 2) & (u2 & 3)) >> 1) | (((u1 >> 2) & (u2 & 3)) & 1)) & ((((u2 >> 2) & (u1 & 3)) >> 1) | (((u2 >> 2) & (u1 & 3)) & 1)));*/
